e79d96307d1fbb49b5534259c2bacfb9a662d764
Cassiano Morais <88691842+Cassiuss17@users.noreply.github.com>
2026-04-10 16:47:00 -0300

    Add files via upload

---
+++ src/utils/content-type.ts
@@ -0,0 +1,25 @@
+export enum ContentType {
+    JSON = "application/json",
+    HTML = "text/html",
+    PLAIN_TEXT = "text/plain",
+
+    XML = "application/xml",
+    FORM_URLENCODED = "application/x-www-form-urlencoded",
+    MULTIPART_FORM_DATA = "multipart/form-data",
+
+    JAVASCRIPT = "application/javascript",
+    CSV = "text/csv",
+
+    OCTET_STREAM = "application/octet-stream",
+
+    PDF = "application/pdf",
+    ZIP = "application/zip",
+
+    IMAGE_JPEG = "image/jpeg",
+    IMAGE_PNG = "image/png",
+    IMAGE_GIF = "image/gif",
+    IMAGE_SVG = "image/svg+xml",
+
+    AUDIO_MPEG = "audio/mpeg",
+    VIDEO_MP4 = "video/mp4",
+}
