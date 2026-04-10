b0b62145fac3c16a381650020f39d83d2dea0c97
Cassiano Morais <88691842+Cassiuss17@users.noreply.github.com>
2026-04-10 16:53:39 -0300

    Add files via upload

---
+++ dist/data/podcasts.js
@@ -0,0 +1,27 @@
+// src/data/podcasts.json
+var podcasts_default = [
+  {
+    podcastName: "flow",
+    episode: "CBUM - Flow 319",
+    videoId: "pQSuQmUfS30",
+    category: ["sa\xFAde", "bodybuilder", "esporte"]
+  },
+  {
+    podcastName: "flow",
+    episode: "RUBENS BARRICHELLO - Flow 339",
+    videoId: "4KDGTdiOV4I",
+    category: ["corrida", "esporte"]
+  },
+  {
+    podcastName: "flow",
+    episode: "Cassiano - Flow 439",
+    videoId: "000000",
+    category: ["esporte", "programa\xE7\xE3o"]
+  },
+  {
+    podcastName: "venus",
+    episode: "Anita - Venus 49",
+    videoId: "003110",
+    category: ["musica", "artistas"]
+  }
+];
